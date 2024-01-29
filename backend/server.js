60c5febee618d320d24a6e35c996a566e6f3621d
Rishabh-Dixit-SE <dixit.rishabh07@gmail.com>
2024-01-29 11:39:01 +0530

    adding errorhandler in productRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,20 @@ import express  from 'express';
 
 import dotenv  from 'dotenv';
 import connectDB from './config/db.js';
-import router from './routes/productRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 dotenv.config();
 const port=process.env.PORT || 5000;
 connectDB();
 const app=express();
+import { notfound,errorHandler } from './middleware/errorMiddleware.js'; 
 
 
 app.get("/",(req,res)=>{
 res.send('Server js is Running');
 })
 
-app.use('/api/products',router);
+app.use('/api/products',productRoutes);
+app.use(notfound);
+app.use(errorHandler);
 
 app.listen(port,()=>{console.log(`server is running on port ${port}`) });
