0236f0a89a27d8d554824db801fff37d5a00b408
Rishabh-Dixit-SE <dixit.rishabh07@gmail.com>
2024-01-29 10:30:36 +0530

    ProductRoute and  asyncHandler

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,20 @@
 
 import express  from 'express';
 
-import Products  from './data/products.js';
+
 import dotenv  from 'dotenv';
 import connectDB from './config/db.js';
+import router from './routes/productRoutes.js';
 dotenv.config();
 const port=process.env.PORT || 5000;
 connectDB();
 const app=express();
 
 
-
-
-
 app.get("/",(req,res)=>{
 res.send('Server js is Running');
 })
 
-app.get("/api/products",(req,res)=>{
-    res.json(Products);
-})
-
-app.get("/api/products/:id",(req,res)=>{
-    const ProductId=req.params.id;
-    const product=Products.find((x)=>x._id===ProductId);
-    res.json(product);
-
-})
+app.use('/api/products',router);
 
 app.listen(port,()=>{console.log(`server is running on port ${port}`) });
