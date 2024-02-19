155f145ae376db2f45aa3723dd7e7536dc179233
Rishabh-Dixit-SE <dixit.rishabh07@gmail.com>
2024-02-19 23:37:03 +0530

    CART REDUX

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,20 +1,34 @@
-import {useParams} from 'react-router-dom';
+import { useState } from 'react';
+import {useParams,useNavigate} from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import {Row,Col,Image,ListGroup,Card,Button, ListGroupItem} from "react-bootstrap"
+import {Form,Row,Col,Image,ListGroup,Card,Button, ListGroupItem} from "react-bootstrap"
 import React from 'react'
 import Rating from '../component/Rating';
 import { useGetProductDetailsQuery } from '../slices/productsApiSlices';
 import Loader from '../component/Loader';
 import Message from '../component/Message';
+import {addToCart} from '../slices/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const ProductScreen = () => {
 
     //url have id that will goe inside productId
 const {id:productId} = useParams();
 
+const dispatch=useDispatch();
+const navigate=useNavigate();
+
+
+
+//for redux cart
+const [qty,setQty]=useState(1);
+
 const {data:product,isLoading,error}=useGetProductDetailsQuery(productId);
 
-
+const addToCartHandler=()=>{
+    dispatch(addToCart({...product,qty}))
+    navigate('/cart');
+    }
 
 {/*
 here we are  using useEffect that why we don't need to use array
@@ -52,10 +66,28 @@ const product=products.find((x)=>x._id===productId) */}
          <ListGroup.Item>
          <h5> Description: {product.description}</h5>
          </ListGroup.Item>
+{/*for redux*/}
+
+{ product.countInStock>0 && (
+    <ListGroup.Item>
+        <Row>
+            <Col><h5>Qty</h5></Col>
+            <Col>
+            <Form.Control as='select' value={qty} onChange={(e)=>setQty(Number(e.target.value))}>
+            { [...Array(product.countInStock).keys()].map((x)=>
+                (<option key={x+1} value={x+1}>
+                     {x+1}
+                </option>)
+            )}
+            </Form.Control>
+            </Col>
+        </Row>
+    </ListGroup.Item>
+) }
 
 <ListGroup.Item>
 
-<Button className='btn-block' type='button' disabled={product.countInStock === 0}>Add To Cart</Button>
+<Button className='btn-block' type='button' disabled={product.countInStock === 0} onClick={addToCartHandler}>Add To Cart</Button>
 </ListGroup.Item>
 </ListGroup>  
 </Col>
