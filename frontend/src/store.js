038dddf20a11e1ae0160cab7b72a9154055c1611
Rishabh-Dixit-SE <dixit.rishabh07@gmail.com>
2024-02-19 19:48:40 +0530

    reduc

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import { apiSlice } from './slices/apiSlice';
+import  createSliceReducer from './slices/cartSlice';
 const store=configureStore({
-    reducer:{[apiSlice.reducerPath]:apiSlice.reducer},
+    reducer:{ [apiSlice.reducerPath]:apiSlice.reducer,
+        cart:createSliceReducer},
+ 
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(apiSlice.middleware),
     devTools:true
     
